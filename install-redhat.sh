c837b0192f631aee59717e51d6f1725eae763c23
Thiago Mallon <jamesmallondev@gmail.com>
2020-07-09 22:17:28 -0800

    fixing flow and identation

--- install-redhat.sh
+++ install-redhat.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Author Thiago Mallon <thiagomallon@gmail.com>
 
+exec 2> logs/error-install.log
+
 vimProf=~/.vim/profile
 vimBndl=~/.vim/profile/bundle
 vimAuto=~/.vim/profile/autoload
@@ -19,12 +21,12 @@ printRed() {
 }
 
 install_go() {
-        GOIVER="$(curl -s https://golang.org/VERSION?m=text)"
-        printOrange "Preparing to install "; printBlue "GO $GOIVER\n"
-        wget https://dl.google.com/go/$GOIVER.linux-amd64.tar.gz
-        sudo tar -C /usr/local -xzf $GOIVER.linux-amd64.tar.gz
-        rm $GOIVER.linux-amd64.tar.gz
-        sh -c 'printf "export GOROOT=/usr/local/go\nexport GOPATH=\$HOME/go\nexport PATH=\$GOPATH/bin:\$GOROOT/bin:\$PATH" >> ~/.profile'
+    GOIVER="$(curl -s https://golang.org/VERSION?m=text)"
+    printOrange "Preparing to install "; printBlue "GO $GOIVER\n"
+    wget https://dl.google.com/go/$GOIVER.linux-amd64.tar.gz
+    sudo tar -C /usr/local -xzf $GOIVER.linux-amd64.tar.gz
+    rm $GOIVER.linux-amd64.tar.gz
+    sh -c 'printf "export GOROOT=/usr/local/go\nexport GOPATH=\$HOME/go\nexport PATH=\$GOPATH/bin:\$GOROOT/bin:\$PATH" >> ~/.profile'
 
 PROGRAMS=("vim" "curl" "git" "go")
 
@@ -32,6 +34,7 @@ for i in "${!PROGRAMS[@]}"
 do
     # ensuring vim installation
     if ! command -v ${PROGRAMS[$i]} &> /dev/null
+    then
         printOrange "\nIt seems that you don't have "; printBlue "${PROGRAMS[$i]} "; 
         printOrange "Would you like to install it now? [Y/n]\n";
 
@@ -44,10 +47,10 @@ do
                 install_go
                 continue
             fi
-            sudo dnf install ${PROGRAMS[$i]}
+            sudo yum install ${PROGRAMS[$i]}
         else
-            printRed "Ok, install vim and run this script again.\n";
-            exit 1
+            printRed "Ok, install ${PROGRAMS[$i]} and run this script again.\n";
+            exit 0
         fi
     fi
 done
@@ -79,5 +82,5 @@ git clone https://github.com/maksimr/vim-jsbeautify
 cd vim-jsbeautify && git submodule update --init --recursive
 
 # ask for set compiled vim as the system default text editor
-printOrange "Select the system default text editor:\n"
-sudo update-alternatives --config editor
+#printOrange "Select the system default text editor:\n"
+#sudo update-alternatives --config editor
